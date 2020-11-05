e23cf6c98d6206fb937086155a22a32cb521e891
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-11-05 19:03:10 +0100

    wyjątki kontrolowane

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,14 @@
 public class Test {
     public static void main(String[] args) {
-        Person person = new Person("Jan", "Kowalski", 25, "1234567890");
-        System.out.println(person);
-        person.setAge(-10);
-        System.out.println(person);
+        Person person;
+        try {
+            person = new Person("Jan", "null", 25, "249167");
+            System.out.println(person);
+            person.setAge(-10);
+            System.out.println(person);
+        } catch (NameUndefinedException | IncorrectAgeException e) {
+            System.out.println(e.getMessage() + "\nKoniec programu.");
+        }
+
     }
 }
